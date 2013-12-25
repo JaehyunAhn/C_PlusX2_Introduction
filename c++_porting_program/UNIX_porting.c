1f087b1420189e356b1b24c0d6a42196c8b0ed98
sogo <sogosonnet@naver.com>
2013-12-25 16:29:55 -0500

    Unix Porting program

--- c++_porting_program/UNIX_porting.c
+++ c++_porting_program/UNIX_porting.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int main () {
-	clock_t now;
-	struct tm * mac_time;
+	clock_t now; //unsigned long 
+	struct tm * mac_time; // const long
 	char *time_str;
 	now = time (NULL);
-	mac_time = localtime(&now);
+	mac_time = localtime((const long *)&now); // localtime ( const long <- unsigned long)
 	time_str = asctime(mac_time);
 	printf("Now : %s\n",time_str);
 	return 0;
