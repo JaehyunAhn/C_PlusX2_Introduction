f3a8171906fc9abdf2de08615f530da77b0a1b27
sogo <sogosonnet@naver.com>
2013-12-31 18:28:14 -0500

    basic in out

---
+++ c++_basic/basic.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	int i;
+	std::cout << "Hello world" << std::endl;
+	std::cout << "Enter a number : ";
+	std::cin >> i;
+	std::cout << i << std::endl;
+	return 0;
+}
