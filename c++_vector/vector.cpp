eefe3d06deb8e812f3787c839c2979176530e217
sogo <sogosonnet@naver.com>
2014-01-14 18:44:32 -0500

    vector operator overloading

--- c++_vector/vector.cpp
+++ c++_vector/vector.cpp
@@ -8,6 +8,8 @@ class Vector {
 	public: // constructor sets all to zero if no args
 		Vector (float first=0.0, float second=0.0, float third=0.0);
 		Vector& operator -= (float scalar);
+		Vector& operator +  (Vector other);
+		void add (Vector first, Vector second);
 		void decrement (float scalar);
 		void print_out();
 	private:
@@ -27,6 +29,18 @@ Vector& Vector::operator -= (float scalar) {
 	return *this;
 }
 
+Vector& Vector::operator + (Vector other) {
+	Vector result; // no constructor: default is all zeros
+	for (int i=0; i < n_dim ; i++)
+		result.data[i] = data[i] + other.data[i];
+	return result;
+}
+
+void Vector::add (Vector first, Vector second) {
+	for (int i=0; i < n_dim ; i++)
+		data[i] = first.data[i] + second.data[i];
+}
+
 void Vector::decrement (float scalar) {
 	int i;
 	for( i=0; i < n_dim; i++)
@@ -42,11 +56,29 @@ void Vector::print_out () {
 int main () {
 	Vector velocity (100.0, 37.0, 500.6);
 	velocity.print_out();
+	cout << endl;
 
 	velocity -= 25;
 	velocity.print_out();
+	cout << endl;
 
 	velocity.decrement(20);
 	velocity.print_out();
+	cout << endl;
+
+	Vector accel (-1.0, 1.0, 0.0), final_velocity;
+	final_velocity = velocity + accel;
+	final_velocity.print_out();
+	cout << endl;
+	
+	final_velocity.add (velocity, accel);
+	final_velocity.print_out();
+	cout << endl;
+	
+	accel -= 10;
+	final_velocity.add (final_velocity, accel);
+	final_velocity.print_out();
+	cout << endl;
+	
 	return 0;
 }
